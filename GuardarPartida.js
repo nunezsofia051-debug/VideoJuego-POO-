b82581e65fc4b533acc312b410882e1e03adedbd
Sofia Nuñez <nunezsofia051@gmail.com>
2026-06-03 19:18:16 -0300

    Agregue Guardar partida y una tienda de juego

---
+++ GuardarPartida.js
@@ -0,0 +1,24 @@
+class Partida {
+
+    constructor(jugador, nivel) {
+        this.jugador = jugador;
+        this.nivel = nivel;
+    }
+
+    guardarPartida() {
+        console.log("Partida guardada correctamente.");
+        console.log("Jugador:", this.jugador);
+        console.log("Nivel:", this.nivel);
+    }
+
+    cargarPartida() {
+        console.log("Cargando partida guardada...");
+        console.log("Bienvenido nuevamente " + this.jugador);
+    }
+
+}
+
+const partida1 = new Partida("Sofia", 10);
+
+partida1.guardarPartida();
+partida1.cargarPartida();
---
+++ TiendaDeJuego.js
@@ -0,0 +1,37 @@
+class Tienda {
+
+    constructor(monedas) {
+        this.monedas = monedas;
+        this.inventario = [];
+    }
+
+    comprarObjeto(objeto, precio) {
+
+        if (this.monedas >= precio) {
+
+            this.monedas -= precio;
+            this.inventario.push(objeto);
+
+            console.log("Compraste: " + objeto);
+            console.log("Monedas restantes: " + this.monedas);
+
+        } else {
+
+            console.log("No tienes monedas suficientes.");
+
+        }
+
+    }
+
+    mostrarInventario() {
+        console.log("Inventario:", this.inventario);
+    }
+
+}
+
+const tienda1 = new Tienda(500);
+
+tienda1.comprarObjeto("Espada Legendaria", 300);
+tienda1.comprarObjeto("Poción", 100);
+
+tienda1.mostrarInventario();
