cc9f609f065f1fb291938967b549f8ab0931c9db
Sofia Nuñez <nunezsofia051@gmail.com>
2026-06-24 17:05:45 -0300

    actualice mision

--- clases/Mision.js
+++ clases/Mision.js
@@ -14,20 +14,4 @@ class Mision {
 
 }
 
-module.exports = Mision;class Mision {
-
-    constructor(nombre, recompensa) {
-        this.nombre = nombre;
-        this.recompensa = recompensa;
-    }
-
-    completarMision() {
-
-        console.log("Misión completada");
-        console.log("Recompensa obtenida: " + this.recompensa);
-
-    }
-
-}
-
 module.exports = Mision;
